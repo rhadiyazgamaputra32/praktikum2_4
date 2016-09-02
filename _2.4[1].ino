b5a507f114b81e66bc22bf3e866a4f25cbc22fb7
rhadiyazgamaputra32 <radiasgama@gmail.com>
2016-09-02 11:28:57 +0700

    praktikum2_4
    praktikum2_4

---
+++ _2.4[1].ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode (A0, INPUT);
+digitalWrite(A0,1);
+pinMode (12, 1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (digitalRead(A0)==LOW){
+      digitalWrite(12,1);
+}
+else {
+        digitalWrite(12,LOW);
+}
+}
